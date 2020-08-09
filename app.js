93dc496dec34e0e73dbca144d5f2d2cc14081bab
YutoSuzuki1024 <yuto.suzuki1024@gmail.com>
2020-08-09 18:52:48 +0900

    　[Finish]2

---
+++ app.js
@@ -0,0 +1,55 @@
+// var hello = 'Hello World';
+// alert(hello);
+
+// alert(4 * 3);
+
+// alert("Hello" + "World");
+
+// var orange = 100;
+// var apple = 120;
+
+// if (orange < apple) {
+// 	alert("みかんの値段がリンゴより安い");
+// }
+
+// else if (orange == apple) {
+// 	alert("みかんとリンゴが同じ値段");
+// }
+
+// else {
+// 	alert("みかんの値段がリンゴより高い");
+// }
+
+// var max = 1900;
+// var num = 1;
+// var count = 0;
+
+// while (num < max) {
+// 	num = num * 2;
+// 	count = count + 1;
+// }
+
+// alert("2を掛け続けて" + max + "を超えるのに必要だった回数は" + count + "回です");
+
+// var i;
+// var num = 0;
+
+// for (i = 1; i < 11; i++) {
+// 	num = num + i;
+// }
+
+// alert("1から10まで足し算した結果は" + num + "です");
+
+var num1 = 8;
+var num2 = 4;
+var sum1 = num1 + num2;
+var sum2 = num1 - num2;
+var sum3 = num1 * num2;
+var sum4 = num1 / num2;
+
+
+
+alert("8 + 4 =" + sum1);
+alert("8 - 4 =" + sum2);
+alert("8 * 4 =" + sum3);
+alert("8 / 4 =" + sum4);
