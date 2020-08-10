68fd8fe863904ae8a035265a7fc022f40eee169d
YutoSuzuki1024 <yuto.suzuki1024@gmail.com>
2020-08-10 17:12:23 +0900

    Finish JS3

--- app.js
+++ app.js
@@ -40,16 +40,171 @@
 
 // alert("1から10まで足し算した結果は" + num + "です");
 
-var num1 = 8;
-var num2 = 4;
-var sum1 = num1 + num2;
-var sum2 = num1 - num2;
-var sum3 = num1 * num2;
-var sum4 = num1 / num2;
+// var num1 = 8;
+// var num2 = 4;
+// var sum1 = num1 + num2;
+// var sum2 = num1 - num2;
+// var sum3 = num1 * num2;
+// var sum4 = num1 / num2;
 
 
 
-alert("8 + 4 =" + sum1);
-alert("8 - 4 =" + sum2);
-alert("8 * 4 =" + sum3);
-alert("8 / 4 =" + sum4);
+// alert("8 + 4 =" + sum1);
+// alert("8 - 4 =" + sum2);
+// alert("8 * 4 =" + sum3);
+// alert("8 / 4 =" + sum4);
+
+// var alertString;
+// alertString = addString("WebCamp");
+
+// alert(alertString);
+
+// function addString (strA) {
+// 	var addStr = "Hello " + strA;
+// 	return addStr;
+// }
+
+// var promptStr = prompt ("何か好きな文字を入力してください。");
+
+// alert(promptStr);
+
+
+
+
+
+//以下確認問題ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+// var user_hand = prompt ("じゃんけんの手をグー、チョキ、パーから選んでください。");
+
+// var js_hand = getJSgand();
+
+// var judge = winLose (user_hand, js_hand);
+
+
+// if (user_hand == "グー" || user_hand == "チョキ" || user_hand == "パー") {
+// 	alert("あなたの選んだ手は" + user_hand + "です。 \nJanaScriptの選んだ手は" + js_hand + "です。　\n結果は" + judge + "です。");
+// }
+// else if (user_hand == null) {
+// 	alert("またチャレンジしてね");
+// }
+// else {
+// 	alert("グー・チョキ・パーのいずれかを入力してください");
+// }
+
+// function getJSgand(){
+// 	var js_hand_num = Math.floor(Math.random() * 3);
+// 	var hand_name;
+
+// 	if (js_hand_num == 0) {
+// 		hand_name = "グー";
+// 	}
+// 	else if (js_hand_num == 1) {
+// 		hand_name = "チョキ";
+// 	}
+// 	else if (js_hand_num == 2) {
+// 		hand_name = "パー";
+// 	}
+
+// 	return hand_name;
+// }
+
+// function winLose (user, js) {
+// 	var winLoseStr;
+
+// 	if (user == "グー") {
+// 		if (js == "グー") {
+// 			winLoseStr = "あいこ";
+// 		}
+// 		else if (js == "チョキ") {
+// 			winLoseStr = "勝ち";
+// 		}
+// 		else if (js == "パー") {
+// 			winLoseStr = "負け";
+// 		}
+// 	}
+
+// 	else if (user == "チョキ") {
+// 		if (js == "グー") {
+// 			winLoseStr = "負け";
+// 		}
+// 		else if (js == "チョキ") {
+// 			winLoseStr = "あいこ";
+// 		}
+// 		else if (js == "パー") {
+// 			winLoseStr = "勝ち";
+// 		}
+// 	}
+
+// 	else if (user == "パー") {
+// 		if (js == "グー") {
+// 			winLoseStr = "勝ち";
+// 		}
+// 		else if (js == "チョキ") {
+// 			winLoseStr = "負け";
+// 		}
+// 		else if (js == "パー") {
+// 			winLoseStr = "あいこ";
+// 		}
+// 	}
+
+// 	return winLoseStr;
+// }
+
+
+
+var user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+  alert('グー・チョキ・パーのいずれかを入力して下さい');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+}
+var js_hand = getJShand();
+var judge = winLose(user_hand, js_hand);
+ 
+if (user_hand != null) {
+    alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+} else {
+    alert("またチャレンジしてね")
+}
+ 
+function getJShand(){
+    var js_hand_num = Math.floor( Math.random() * 3 );
+    var hand;
+    if(js_hand_num == 0){
+        hand = "グー";
+    }else if(js_hand_num == 1){
+        hand = "チョキ";
+    }else if(js_hand_num == 2){
+        hand = "パー";
+    }
+    return hand;
+}
+ 
+function winLose(user, js){
+    var winLoseStr;
+    if(user == "グー"){
+        if(js == "グー"){
+            winLoseStr = "あいこ";
+        }else if(js == "チョキ"){
+            winLoseStr = "勝ち";
+        }else if(js == "パー"){
+            winLoseStr = "負け";
+        }
+    }else if(user == "チョキ"){
+        if(js == "グー"){
+            winLoseStr = "負け";
+        }else if(js == "チョキ"){
+            winLoseStr = "あいこ";
+        }else if(js == "パー"){
+            winLoseStr = "勝ち";
+        }
+    }else if(user == "パー"){
+        if(js == "グー"){
+            winLoseStr = "勝ち";
+        }else if(js == "チョキ"){
+            winLoseStr = "負け";
+        }else if(js == "パー"){
+            winLoseStr = "あいこ";
+        }
+    }
+    return winLoseStr;
+}
